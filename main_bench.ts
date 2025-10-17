d62c67f2cc01fc093b6ffff45a6aafbf1eef760f
Felipe Santos <felipecassiors@gmail.com>
2025-10-17 09:49:14 -0300

    Increase number of benchmark iterations

--- main_bench.ts
+++ main_bench.ts
@@ -3,37 +3,61 @@ import { parse } from "@std/jsonc";
 import { parse as parseJsoncMorph } from "jsonc-morph";
 import { assertEquals } from "@std/assert";
 
-Deno.bench("parse and export", async (b) => {
-  const expected = await Deno.readTextFile("./fixtures/object/expected.jsonc");
+Deno.bench(
+  "parse and export",
+  {
+    n: 10_000,
+  },
+  async (b) => {
+    const expected = await Deno.readTextFile(
+      "./fixtures/object/expected.jsonc"
+    );
 
-  b.start();
-  const parsed = parseJsoncMorph(expected);
-  const result = parsed.toString();
-  b.end();
+    b.start();
+    const parsed = parseJsoncMorph(expected);
+    const result = parsed.toString();
+    b.end();
 
-  assertEquals(result, expected);
-});
+    assertEquals(result, expected);
+  }
+);
 
-Deno.bench("object", async (b) => {
-  const original = await Deno.readTextFile("./fixtures/object/original.jsonc");
-  const expected = await Deno.readTextFile("./fixtures/object/expected.jsonc");
-  const modified = parse(expected) as object;
+Deno.bench(
+  "object",
+  {
+    n: 10_000,
+  },
+  async (b) => {
+    const original = await Deno.readTextFile(
+      "./fixtures/object/original.jsonc"
+    );
+    const expected = await Deno.readTextFile(
+      "./fixtures/object/expected.jsonc"
+    );
+    const modified = parse(expected) as object;
 
-  b.start();
-  const result = weave(original, modified);
-  b.end();
+    b.start();
+    const result = weave(original, modified);
+    b.end();
 
-  assertEquals(result, expected);
-});
+    assertEquals(result, expected);
+  }
+);
 
-Deno.bench("array", async (b) => {
-  const original = await Deno.readTextFile("./fixtures/array/original.jsonc");
-  const expected = await Deno.readTextFile("./fixtures/array/expected.jsonc");
-  const modified = parse(expected) as unknown[];
+Deno.bench(
+  "array",
+  {
+    n: 10_000,
+  },
+  async (b) => {
+    const original = await Deno.readTextFile("./fixtures/array/original.jsonc");
+    const expected = await Deno.readTextFile("./fixtures/array/expected.jsonc");
+    const modified = parse(expected) as unknown[];
 
-  b.start();
-  const result = weave(original, modified);
-  b.end();
+    b.start();
+    const result = weave(original, modified);
+    b.end();
 
-  assertEquals(result, expected);
-});
+    assertEquals(result, expected);
+  }
+);
