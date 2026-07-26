7822935478791e8b768fd41ba24bea661c7b2bda
Felipe Santos <felipecassiors@gmail.com>
2026-07-26 16:44:20 -0300

    ci: add semantic-release for npm and JSR
    Automate versioning and dual publishing with semantic-release,
    matching bindl's release config while also publishing to JSR via
    @sebbo2002/semantic-release-jsr. Build the npm package with dnt
    before release; version is owned by git tags going forward.

---
+++ release.config.js
@@ -0,0 +1,80 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        releaseRules: [
+          {
+            type: "refactor",
+            release: "patch",
+          },
+          {
+            type: "perf",
+            release: "patch",
+          },
+          {
+            type: "build",
+            scope: "deps",
+            release: "patch",
+          },
+          // https://github.com/semantic-release/commit-analyzer/issues/413#issuecomment-1465299187
+          {
+            breaking: true,
+            release: "major",
+          },
+        ],
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        presetConfig: {
+          types: [
+            {
+              type: "feat",
+              section: "Features",
+            },
+            {
+              type: "fix",
+              section: "Bug Fixes",
+            },
+            {
+              type: "perf",
+              section: "Performance Improvements",
+            },
+            {
+              type: "revert",
+              section: "Reverts",
+            },
+            {
+              type: "refactor",
+              section: "Code Refactoring",
+            },
+            {
+              type: "build",
+              scope: "deps",
+              section: "Dependencies",
+            },
+          ],
+        },
+      },
+    ],
+    [
+      "@semantic-release/npm",
+      {
+        pkgRoot: "npm",
+      },
+    ],
+    "@sebbo2002/semantic-release-jsr",
+    [
+      "@semantic-release/github",
+      {
+        addReleases: "bottom",
+      },
+    ],
+  ],
+  preset: "conventionalcommits",
+};
